c46e80ec2432418ed8ba6baa34cc4e50506bb123
Séyhan A. Cileli <52051065+seycileli@users.noreply.github.com>
2020-07-04 22:32:00 +0300

    Create DialogController.java

---
+++ controller/DialogController.java
@@ -0,0 +1,33 @@
+package controller;
+
+import datamodel.ToDoData;
+import datamodel.ToDoItem;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.time.LocalDate;
+
+public class DialogController {
+
+    @FXML
+    private TextField shortDescriptionField;
+
+    @FXML
+    private TextArea detailsArea;
+
+    @FXML
+    private DatePicker deadlinePicker;
+
+    public ToDoItem processResults() {
+        String shortDescription = shortDescriptionField.getText().trim();
+        String details = detailsArea.getText().trim();
+        LocalDate deadlineValue = deadlinePicker.getValue();
+
+
+        ToDoItem newItem = new ToDoItem(shortDescription, details, deadlineValue);
+        ToDoData.getInstance().addTodoItem(newItem);
+        return newItem;
+    }
+}
